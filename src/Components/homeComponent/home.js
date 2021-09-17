5ee693ac7c6869164b1203511c3af1cc76409e8a
Roz <rosalindavalbrecht@gmail.com>
2021-09-16 17:04:34 -0700

    bare bones assets and text in jsx

---
+++ src/Components/homeComponent/home.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Hi, I'm Roz</h1>
+      <h2>[fullstack MERN developer]</h2>
+      <img
+        src="/keyboardBlack.jpg"
+        alt="Black keyboard with brightly colored LEDs shining behind the keys."
+      />
+
+      <hr></hr>
+      <h2>I make things</h2>
+      <h1>&</h1>
+      <h2>solve problems</h2>
+      <hr></hr>
+
+      <img
+        src="/fiberOpticDark.jpg"
+        alt="Fiber Optic wire backlit by purple blue and green LEDs."
+      />
+
+      <hr></hr>
+      <h1>Ready to level up your website?</h1>
+      <hr></hr>
+
+      <h3>S</h3>
+      <h3>T</h3>
+      <h3>A</h3>
+      <h3>C</h3>
+      <h3>K</h3>
+
+      <h4>
+        React <br></br>
+        React Native <br></br> Mongo <br></br> Express <br></br> Responsive
+        Design <br></br> Node js <br></br> Rest API <br></br> Sass <br></br>
+        Bootstrap <br></br> Figma <br></br> VBA <br></br>
+      </h4>
+
+      <hr></hr>
+      <img 
+      src="/lightBulbDark.jpg"
+      alt="A light bulb backlit by orange and pink fire."
+      />
+      <hr></hr>
+
+      <h2>contact</h2>
+      <h4>github.com/expositionjunkie</h4>
+      <h4>rosalindavalbrecht@gmail.com</h4>
+      <h4>linkedin.com/in/rozalbrecht</h4>
+      
+    </div>
+  );
+}
+
+export default Home;
