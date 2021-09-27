0338650d08919ae85a3b9ee4191a3a79d8c67b26
Roz <rosalindavalbrecht@gmail.com>
2021-09-27 15:21:10 -0700

    animated navbar and fixed some formatting.

--- src/Components/globalComponents/Navbar.js
+++ src/Components/globalComponents/Navbar.js
@@ -1,21 +1,50 @@
 import React, { useState } from "react";
 import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
-import * as CgIcons from "react-icons/cg";
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
 import { IconContext } from "react-icons";
 import "../../Styles/Navbar.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import { useTransition, animated } from "react-spring";
 
 //tutorial nav and routing: https://www.youtube.com/watch?v=CXa0f4-dWi4
 
-/* function ToggleNav() {
-  const [toggle, set] = useState(false)
+function ToggleNav(aDiv, toggle) {
   const transitions = useTransition(toggle, {
+    from: { position: "absolute", opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    delay: 200,
+    config: config.molasses,
+    trail: 3,
+    onProps: (toggle) => (!toggle ? { reset: true, reverse: true } : toggle),
+  });
+  return transitions(({ opacity }, item) =>
+    item ? (
+      <animated.div
+        style={{
+          position: "fixed",
+          left: 0,
+          zIndex: 100,
+          opacity: opacity.to({ range: [0.0, 1.0], output: [0, 1] }),
+          transform: opacity.to((y) => `translate3d(0,${y}px,0)`),
+        }}
+      >
+        {aDiv}
+      </animated.div>
+    ) : (
+      <animated.div
+        style={{
+          left: "-100%",
+          position: "absolute",
+          opacity: opacity.to({ range: [1.0, 0.0], output: [1, 0] }),
+          transform: opacity.to((y) => `translate3d(0,${y}px,0)`),
+        }}
+      >{aDiv}</animated.div>
+    )
+  );
+}
 
-  })
-};
- */
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
@@ -31,20 +60,23 @@ function Navbar() {
       </div>
       <IconContext.Provider value={{ color: "#ffff", size: "5rem" }}>
         <nav className={sidebar ? "nav-menu-active" : "nav-menu"}>
-          <ul className="nav-menu-items" onClick={showSidebar}>
-            {SidebarData.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                  <Link to={item.path}>
-                    <React.Fragment className="nav-icon">
-                      {item.icon}
-                    </React.Fragment>
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+          {ToggleNav(
+            <ul className="nav-menu-items" onClick={showSidebar}>
+              {SidebarData.map((item, index) => {
+                return (
+                  <li key={index} className={item.cName}>
+                    <Link to={item.path}>
+                      <React.Fragment className="nav-icon">
+                        {item.icon}
+                      </React.Fragment>
+                      <span>{item.title}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>,
+            sidebar
+          )}
         </nav>
       </IconContext.Provider>
     </div>
